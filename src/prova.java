0c5c715dbcfce84871d8077df56ad7d0b95772a2
Peppe93 <grosa93@hotmail.it>
2018-01-08 18:29:59 +0100

    first commit

---
+++ src/prova.java
@@ -0,0 +1,13 @@
+
+public class prova {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ciao Github, sono Giuseppe");
+	}
+
+}
